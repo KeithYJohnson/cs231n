bd6c3cee12acb41b579e9378ed5123c50c6a6204
Keith Johnson <keithyjohnson@gmail.com>
2017-01-06 18:08:30 -0500

    Regularizing gradient in svm_loss_naive

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -50,6 +50,7 @@ def svm_loss_naive(W, X, y, reg):
   # to be an average instead so we divide by num_train.
   loss /= num_train
   dW   /= num_train
+  dW   += reg * W
 
   # Add regularization to the loss.
   loss += 0.5 * reg * np.sum(W * W)
