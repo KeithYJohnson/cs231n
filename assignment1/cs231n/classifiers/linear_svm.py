9a8ebdb4cc630a6318325e7afab3bff97af392d6
Keith Johnson <keithyjohnson@gmail.com>
2017-01-06 16:47:16 -0500

    Renaming correct_class_score to score_for_correct_class

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -27,11 +27,11 @@ def svm_loss_naive(W, X, y, reg):
   loss = 0.0
   for i in xrange(num_train):
     scores = X[i].dot(W)
-    correct_class_score = scores[y[i]]
+    score_for_correct_class = scores[y[i]]
     for j in xrange(num_classes):
       if j == y[i]:
         continue
-      margin = scores[j] - correct_class_score + 1 # note delta = 1
+      margin = scores[j] - score_for_correct_class + 1 # note delta = 1
       if margin > 0:
         loss += margin
 
