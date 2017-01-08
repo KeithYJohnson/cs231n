c7f03bfae1627cbfa774a94ebd15fd13edc28743
Keith Johnson <keithyjohnson@gmail.com>
2017-01-07 19:58:39 -0500

    Retrieving a num_train D vector filled with the score for the correct class for each example.

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -86,8 +86,10 @@ def svm_loss_vectorized(W, X, y, reg):
   """
   loss = 0.0
   dW = np.zeros(W.shape) # initialize the gradient as zero
+  num_train = X.shape[0]
 
   scores = X.dot(W)
+  scores_for_correct_classes = scores[np.arange(num_train), y].reshape(num_train,1)
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
