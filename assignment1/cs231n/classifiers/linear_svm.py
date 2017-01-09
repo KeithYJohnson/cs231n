Bugfix.  After computing scores, we need to ensure they are set to zero for cases where y_i(the class for a given row) was compared against itself in the vectorized score calculation. -  The bug was that it was setting this to zero for the `margins`, instead of the `scores`

assignment1/cs231n/classifiers/linear_svm.py
@@ -84,7 +84,7 @@ def svm_loss_vectorized(W, X, y, reg):
   #  take the diff of the correct class with itself, which is always
   #  zero, plus the constant of 1, resulting in 1 getting added to the
   #  error for each example, we set these to zero on the next line.
-  margins[np.arange(num_train), y] = 0
+  scores[np.arange(num_train),y]  = 0
 
   loss  = np.sum(margins)
   loss /= num_train