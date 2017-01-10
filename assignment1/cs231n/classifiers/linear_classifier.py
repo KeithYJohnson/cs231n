d038aa691fa4b5948a2358deecf73e0ceee29d32
Keith Johnson <keithyjohnson@gmail.com>
2017-01-10 15:38:17 -0500

    predicting scores after training.

--- assignment1/cs231n/classifiers/linear_classifier.py
+++ assignment1/cs231n/classifiers/linear_classifier.py
@@ -44,7 +44,7 @@ class LinearClassifier(object):
       loss_history.append(loss)
 
       self.W =+ - learning_rate * grad
-      
+
       if verbose and it % 100 == 0:
         print 'iteration %d / %d: loss %f' % (it, num_iters, loss)
 
@@ -63,15 +63,9 @@ class LinearClassifier(object):
       array of length N, and each element is an integer giving the predicted
       class.
     """
-    y_pred = np.zeros(X.shape[1])
-    ###########################################################################
-    # TODO:                                                                   #
-    # Implement this method. Store the predicted labels in y_pred.            #
-    ###########################################################################
-    pass
-    ###########################################################################
-    #                           END OF YOUR CODE                              #
-    ###########################################################################
+    y_pred = np.zeros(X.shape[0])
+    scores = X.dot(self.W)
+    y_pred = np.argmax(scores, axis=1)
     return y_pred
 
   def loss(self, X_batch, y_batch, reg):
